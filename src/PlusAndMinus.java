486a6d124bc61b627383aecb251d33f4b5eb566f
Chanok Pathompatai <cpathompatai@gmail.com>
2026-06-09 13:42:38 +0700

    add PlusAndMinus.java

--- src/PlusAndMinus.java
+++ src/PlusAndMinus.java
@@ -2,13 +2,25 @@ import java.util.Scanner;
 
 public class PlusAndMinus {
     static int calculate(String text) {
-        
         int idx1 = text.lastIndexOf("-");
         int idx2 = text.lastIndexOf("+");
         // * (idx1 == -1) && (idx2 == -1) 
         if (idx1 == idx2 ) {
+            /**
+             * ! text == "" ให้คืนค่า 0
+             * ! text == "203" ให้คืนค่า 203
+             */
+            if (text.isEmpty()) {
+                return 0;
+            }
             return Integer.parseInt(text);
         }
+        /**
+         * ! ถ้า text == "-542"
+         * ! ได้ว่า left = "" และ right = "-542"
+         * ! ทำให้ next = calculate("") = 0 และ value = -542
+         * ! คืนค่า 0 + (-542) = -542
+         */
 
         int idx = (idx1 < idx2) ? idx2 : idx1;
         
